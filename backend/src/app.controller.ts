f8db754cb5a6aa8aa95cda81b9cf9eb9f337c2bb
Sadat Ahmad <sadatahmadofficial@gmail.com>
2025-06-25 10:03:49 +0200

    refator(backend): improve token-expiry flow

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -50,11 +50,36 @@ export class AppController {
   }
 
   @Get('login')
-  loginOAuth(@Res() res: Response, @Req() req: Request) {
-    req.session?.hubspot?.access_token
-      ? res.redirect('http://localhost:5173/contacts')
-      : res.redirect(
-          `https://app-eu1.hubspot.com/oauth/authorize?client_id=${this.configService.get<string>('CLIENT_ID')}&redirect_uri=http://localhost:3000/oauth-callback&scope=crm.objects.contacts.write%20oauth%20crm.objects.contacts.read`,
+  async loginOAuth(@Res() res: Response, @Req() req: Request) {
+    if (req.session?.hubspot?.access_token) {
+      const isExpired = Date.now() > req.session.hubspot.expires_in;
+      if (isExpired) {
+        const hubspot = req.session.hubspot;
+        const { data } = await firstValueFrom(
+          this.httpService.post(
+            'https://api.hubapi.com/oauth/v1/token',
+            new URLSearchParams({
+              grant_type: 'refresh_token',
+              client_id: this.configService.get<string>('CLIENT_ID'),
+              client_secret: this.configService.get<string>('CLIENT_SECRET'),
+              refresh_token: hubspot.refreshToken,
+            }),
+            {
+              headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            },
+          ),
         );
+        const newToken = data;
+        hubspot.accessToken = newToken.access_token;
+        hubspot.expiresAt = Date.now() + newToken.expires_in * 1000;
+        if (newToken.refresh_token) {
+          hubspot.refreshToken = newToken.refresh_token;
+        }
+      }
+      res.redirect('http://localhost:5173/contacts');
+    } else
+      res.redirect(
+        `https://app-eu1.hubspot.com/oauth/authorize?client_id=${this.configService.get<string>('CLIENT_ID')}&redirect_uri=http://localhost:3000/oauth-callback&scope=crm.objects.contacts.write%20oauth%20crm.objects.contacts.read`,
+      );
   }
 }
